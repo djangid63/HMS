3927de3927e329efd81798640e4579d56a7ed984
djangid63 <jangiddevesh031@gmail.com>
2025-05-05 23:56:56 +0530

    feat: Reorganize booking controller methods for improved readability and structure

--- Backend/Controllers/bookingController.js
+++ Backend/Controllers/bookingController.js
@@ -1,5 +1,22 @@
 const bookingModel = require('../Models/bookingModel')
 
-exports.addBooking = async (req, res) => {
 
+exports.getBooking = async (req, res) => {
+  try {
+    const bookings = await bookingModel.find()
+    res.status(200).json({ status: true, message: "Data found", data: bookings })
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+
+exports.addBooking = async (req, res) => {
+  try {
+    const booking = new bookingModel(req.body);
+    const savedBooking = await booking.save();
+    res.status(201).json(savedBooking);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
